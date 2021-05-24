19d2d7e1c2d0387f7b8fa01fc25d35544b005eba
NB1723 <notalia03@gmail.com>
2021-05-24 15:00:49 +0300

    lab5.1

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest71
+{
+	TEST_CLASS(UnitTest71)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			std::array<int, 10> b{ {2, 5, 21, 0, 1, 0, 1, 0, 0, 0} };
+			double avg = avg_even(b);
+			Assert::AreEqual(avg, 5.);
+		}
+	};
+}
